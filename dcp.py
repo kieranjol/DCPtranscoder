3a58cbc3c1904f926dc5181e61658d1516e74491
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-11-19 10:11:09 +0000

    removed useless imports
    I just did a copy pasta from one of my other scripts

--- dcp.py
+++ dcp.py
@@ -1,14 +1,10 @@
 
 import subprocess
 import sys
-import filecmp
-from glob import glob
 import os
 from Tkinter import *
 import tkFileDialog
 root = Tk()
-import fnmatch
-
 
 my_match = []
 # Create file-open dialog.
@@ -17,9 +13,6 @@ root.update()
 video_dir = tkFileDialog.askdirectory(parent=root)
 os.chdir(video_dir)
 
-
-   
-       
 mxfs = filter(lambda x: x.endswith('.mxf'), os.listdir(video_dir))
 #output is currently within the DCP. This is bad, will add option for output dir later.
 output = mxfs[0] + '.mov'
