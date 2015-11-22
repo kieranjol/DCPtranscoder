6b9c638afc8a89d9fc128bf293c21c78451e71aa
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-11-22 17:52:12 +0000

    Added gui file open, better picture variables

--- testmulti.py
+++ testmulti.py
@@ -1,20 +1,28 @@
 import subprocess
 import sys
 import os
+from Tkinter import Tk
+import tkFileDialog
+root = Tk()
 
-filename = sys.argv[1]
+# Create file-open dialog.
+root.update()
+
+# File open dialog will appear. Whatever directory you choose will be stored as video_dir
+filename = tkFileDialog.askopenfilename(parent=root)
+
 output = filename + '_concattt.mov'
 doc = filename + '.txt'
 wd = os.path.dirname(filename)
-print wd
-acodec_raw1 = subprocess.check_output(['xml', 'sel', '-N', 'x=http://www.digicine.com/PROTO-ASDCP-PKL-20040311#', '-t', '-m', "//x:Asset[contains(x:Type,'Picture')]", '-v', 'x:OriginalFileName', '-n', filename ]).splitlines() 
+
+picture_files = subprocess.check_output(['xml', 'sel', '-N', 'x=http://www.digicine.com/PROTO-ASDCP-PKL-20040311#', '-t', '-m', "//x:Asset[contains(x:Type,'Picture')]", '-v', 'x:OriginalFileName', '-n', filename ]).splitlines() 
 dir_append = wd + '/'
 concat_string = 'file \'' 
 concat_append = '\''
-acodec_raw = [dir_append + x for x in acodec_raw1]
+picture_files_fix1 = [dir_append + x for x in picture_files]
 # http://stackoverflow.com/a/2050721/2188572
-newlist = [concat_string + x for x in acodec_raw]
-finalpic = [x + concat_append for x in newlist]
+picture_files_fix2 = [concat_string + x for x in picture_files_fix1]
+finalpic = [x + concat_append for x in picture_files_fix2]
 print finalpic
 #http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
 file = open(doc, "w")
@@ -26,7 +34,7 @@ file.close()
 docaudio = filename + 'audio.txt'
 outputaudio = filename + '.wav'
 wd = os.path.dirname(filename)
-print wd
+
 audio = subprocess.check_output(['xml', 'sel', '-N', 'x=http://www.digicine.com/PROTO-ASDCP-PKL-20040311#', '-t', '-m', "//x:Asset[contains(x:Type,'Sound')]", '-v', 'x:OriginalFileName', '-n', filename ]).splitlines() 
 dir_append = wd + '/'
 concat_string = 'file \'' 
