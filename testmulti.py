befe347c9f39aebe73aec181389b39fed66c9aa9
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-11-22 20:56:21 +0000

    Better variables, cleanup, comments

--- testmulti.py
+++ testmulti.py
@@ -11,11 +11,22 @@ root.update()
 # File open dialog will appear. Whatever directory you choose will be stored as video_dir
 filename = tkFileDialog.askopenfilename(parent=root)
 
+# Set variables for various filenames.
 output = filename + '_concattt.mov'
-doc = filename + '.txt'
+video_concat_textfile = filename + '.txt'
 wd = os.path.dirname(filename)
+audio_concat_textfile = filename + 'audio.txt'
+outputaudio = filename + '.wav'
+outputmuxed = filename + '_muxed.mov'
 
-picture_files = subprocess.check_output(['xml', 'sel', '-N', 'x=http://www.digicine.com/PROTO-ASDCP-PKL-20040311#', '-t', '-m', "//x:Asset[contains(x:Type,'Picture')]", '-v', 'x:OriginalFileName', '-n', filename ]).splitlines() 
+# Search through XML for filenames containing picture.
+picture_files = subprocess.check_output(['xml', 'sel', 
+                                         '-N', 'x=http://www.digicine.com/PROTO-ASDCP-PKL-20040311#',
+                                         '-t', '-m', "//x:Asset[contains(x:Type,'Picture')]",
+                                         '-v', 'x:OriginalFileName',
+                                         '-n', filename ]).splitlines() 
+
+# Transformations to picture_files in order to fit the ffmpeg concat text standard.
 dir_append = wd + '/'
 concat_string = 'file \'' 
 concat_append = '\''
@@ -23,45 +34,48 @@ picture_files_fix1 = [dir_append + x for x in picture_files]
 # http://stackoverflow.com/a/2050721/2188572
 picture_files_fix2 = [concat_string + x for x in picture_files_fix1]
 finalpic = [x + concat_append for x in picture_files_fix2]
-print finalpic
-#http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
-file = open(doc, "w")
+
+# Write the list of filenames containing picture to a textfile. 
+# http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
+file = open(video_concat_textfile, "w")
 for item in finalpic:
   file.write("%s\n" % item)
-file.close()  
+file.close()  # ffmpeg can't access the textfile until it's closed.
 
+# Search through XML for filenames containing audio.
+audio_files = subprocess.check_output(['xml', 'sel', '-N', 'x=http://www.digicine.com/PROTO-ASDCP-PKL-20040311#', '-t', '-m', "//x:Asset[contains(x:Type,'Sound')]", '-v', 'x:OriginalFileName', '-n', filename ]).splitlines() 
 
-docaudio = filename + 'audio.txt'
-outputaudio = filename + '.wav'
-wd = os.path.dirname(filename)
-
-audio = subprocess.check_output(['xml', 'sel', '-N', 'x=http://www.digicine.com/PROTO-ASDCP-PKL-20040311#', '-t', '-m', "//x:Asset[contains(x:Type,'Sound')]", '-v', 'x:OriginalFileName', '-n', filename ]).splitlines() 
+# Transformations to picture_files in order to fit the ffmpeg concat text standard.
 dir_append = wd + '/'
 concat_string = 'file \'' 
 concat_append = '\''
-audio_raw = [dir_append + x for x in audio]
+audio_raw = [dir_append + x for x in audio_files]
 # http://stackoverflow.com/a/2050721/2188572
 newaudio = [concat_string + x for x in audio_raw]
 finalaudio = [x + concat_append for x in newaudio]
-print finalaudio
-#http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
-file = open(docaudio, "w")
+
+# Write the list of filenames containing picture to a textfile. 
+# http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
+file = open(audio_concat_textfile, "w")
 for item in finalaudio:
   file.write("%s\n" % item)
 file.close()  
-outputmuxed = filename + '_muxed.mov'
 
+# Call ffmpeg to join all audio files into one WAV file
 subprocess.call(['ffmpeg',
                          '-f', 'concat',
-                         '-i', docaudio,
+                         '-i', audio_concat_textfile,
                          '-c:a', 'copy',
                          outputaudio])
+                         
+# Call ffmpeg to join all video files into one prores.mov
 subprocess.call(['ffmpeg',
                          '-f', 'concat',
-                         '-i', doc,
+                         '-i', video_concat_textfile,
                          '-c:v', 'prores',
                          output])
-                         
+
+# Call ffmpeg to create one final muxed audiovisual asset in a .mov wrapper.
 subprocess.call(['ffmpeg',
                          '-i', output,
                          '-i', outputaudio,
